a90825ce740020dbe4e8ce461625dd4bddb3f64e
Simon Johnston <simonjohnston@Simons-MacBook-Air.local>
2021-04-02 18:37:22 -0600

    inital

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -5,14 +5,11 @@ import LightTheme from '../themes/Light'
 const Navigator = ({theme}) => {
   return (
   <Navbar collapseOnSelect expand="lg" bg={theme == LightTheme ? "light":"dark"} variant={theme == LightTheme ? "light":"dark"} >
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="/">Cheat Sheet</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="justify-content-end">
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
+      <Nav.Link href="/about">About</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Button,ProgressBar} from 'react-bootstrap'
+import styled from 'styled-components'
 
 const Home = () => {
+
+  const [user,setUser] = useState(true)
+
+  const changeUser = () => setUser(!user)
+
+
+
   return (
-    <div>
+    <JaredsBox>
+      <div>
       <h1>This the the home</h1>
-    </div>
+      <button style={{marginLeft:'10px'}}>Button 1</button>
+      <Button onClick={changeUser}>Button 2</Button>
+      <StyledButton>Button 3</StyledButton>
+      <StyledBSbutton>Button 4</StyledBSbutton>
+      </div>
+      <div>
+      <ProgressBar variant="success" now={40} />
+      <ProgressBar variant="info" now={20} />
+      <ProgressBar variant="warning" now={60} />
+      <ProgressBar variant="danger" now={80} />
+      </div>
+      <div>
+        <StyledProgress now={80}/>
+      </div>
+      {user == true ? <Button>Edit Post</Button>: <h6>nothing see</h6>}
+      {/* {user.id == postMessage.user_id ? <Button>Edit Post</Button>: <h6>nothing see</h6>} */}
+    </JaredsBox>
+  
   )
 }
+
+
+const JaredsBox = styled.div`
+  padding:10%;
+  background:yellow;
+  display:flex;
+`
+
+const StyledButton = styled.button` 
+margin:10px;
+color:red;
+background:blue;
+border-radius: 15px;
+`
+
+const StyledBSbutton = styled(Button)`
+margin:10px;
+background:purple;
+&:hover{
+  background:red;
+}
+`
+const StyledProgress = styled(ProgressBar)`
+margin:10px;
+.progress-bar{
+  background-color: purple;
+}
+`
+
+
 
 export default Home
